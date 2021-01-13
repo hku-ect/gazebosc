caac9df686c0a7e9eb7c3c0a3481855d085407ee
Arnaud Loonstra <arnaud@sphaero.org>
2021-01-13 11:41:04 +0100

    fixes for cmake not finding zmq

--- Findlibzmq.cmake
+++ Findlibzmq.cmake
@@ -5,7 +5,7 @@
 
 if (NOT MSVC)
     include(FindPkgConfig)
-    pkg_check_modules(PC_${CMAKE_FIND_PACKAGE_NAME} "libzmq")
+    pkg_check_modules(PC_LIBZMQ "libzmq")
     if (PC_LIBZMQ_FOUND)
         # add CFLAGS from pkg-config file, e.g. draft api.
         add_definitions(${PC_LIBZMQ_CFLAGS} ${PC_LIBZMQ_CFLAGS_OTHER})
