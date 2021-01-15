d88302a89e9e1c477aa1d4510df4966654a09d67
Aaron Oostdijk <aoostdijk@gmail.com>
2021-01-15 11:52:02 +0100

    reverted incorrect appveyor edit, tweaked build script

--- build_windows.bat
+++ build_windows.bat
@@ -1,5 +1,3 @@
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
-
 set PROJECT_ROOT=%cd%
 
 set INSTALL_PREFIX=%cd%/tmp/ci_build
