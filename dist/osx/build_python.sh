476bbccdf2b570682b7f0711005fdef614ad0802
Arnaud Loonstra <arnaud@sphaero.org>
2021-02-18 09:44:23 +0100

    try framework instead

--- dist/osx/build_python.sh
+++ dist/osx/build_python.sh
@@ -3,6 +3,6 @@ set -ev
 ROOT=`pwd`
 git clone https://github.com/python/cpython.git --branch=3.8 --depth=1
 cd cpython
-./configure --enable-shared --with-pydebug --with-openssl=$(brew --prefix openssl) --prefix=$ROOT/pyroot
+./configure --enable-framework=$HOME/Library/Frameworks --with-pydebug --with-openssl=$(brew --prefix openssl) --prefix=$ROOT/pyroot
 make -s
 make install > /dev/null
