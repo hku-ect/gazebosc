ae1e93c9a94bb91126a0108310e84588ed88a5e8
Arnaud Loonstra <arnaud@sphaero.org>
2021-10-07 21:16:11 +0200

    single line process command we split on space char

--- Actors/ProcessActor.cpp
+++ Actors/ProcessActor.cpp
@@ -31,10 +31,36 @@ s_init_zproc(zproc_t **proc_p)
     zproc_set_stdout(*proc_p, NULL);
 }
 
+zlist_t *
+s_string_split(char *stringtosplit, const char delim)
+{
+    assert(stringtosplit);
+    assert(delim);
+    const char d[2] = { delim, (char)0x0 };
+    zlist_t *retlst = zlist_new();
+    zlist_autofree(retlst);
+
+    //  get the first token */
+    char *token = strtok(stringtosplit, d);
+    if (token == NULL)
+    {
+        zlist_append(retlst, stringtosplit);
+    }
+    while (token != NULL)
+    {
+        zsys_info("%s", token);
+        zlist_append(retlst, token);
+        token = strtok(NULL, d);
+    }
+    // free(stringtosplit); the string is modified into the splitted string so will be freed when the zlist is freed
+    return retlst;
+}
+
 zmsg_t * ProcessActor::handleInit(sphactor_event_t *ev)
 {
     sphactor_actor_set_capability((sphactor_actor_t*)ev->actor, zconfig_str_load(processactorcapabilities));
     s_init_zproc(&this->proc);
+    zproc_set_verbose(this->proc, true);
 
     return nullptr;
 }
@@ -65,23 +91,32 @@ zmsg_t * ProcessActor::handleAPI(sphactor_event_t *ev)
         assert(arg);
         if (strlen( arg ) )
         {
+            // are we polling on stdout?
+            sphactor_actor_poller_remove( (sphactor_actor_t *)ev->actor, zproc_stdout(this->proc) );
             s_init_zproc(&this->proc);
-            zlist_t *arglist = zlist_new();
-            zlist_autofree (arglist);
-            zlist_append(arglist, (void *)arg);
-            // get additional args
-            arg = zmsg_popstr(ev->msg);
+            zlist_t *arglist = s_string_split(arg, ' ');
+            // get additional args NOT USED we get a single string with the full command
+            /*arg = zmsg_popstr(ev->msg);
             while (arg)
             {
                 zlist_append(arglist, (void *)arg);
                 arg = zmsg_popstr(ev->msg);
-            }
+            }*/
             zproc_set_args( this->proc, &arglist);
             int rc = zproc_run(this->proc);
-            assert(rc == 0);
-            // poll on the stdout socket
-            sphactor_actor_poller_add((sphactor_actor_t *)ev->actor, zproc_stdout(this->proc));
-
+            if (rc != 0)
+            {
+                //TODO: set error state
+                zsys_error("Failed to run command %s", (char *)zlist_first(zproc_args(this->proc)));
+            }
+            else
+            {
+                // poll on the stdout socket
+                if ( zproc_running )
+                    sphactor_actor_poller_add((sphactor_actor_t *)ev->actor, zproc_stdout(this->proc));
+                else
+                    zsys_info("the process has already finished");
+            }
         }
         else
             zstr_free(&arg);
