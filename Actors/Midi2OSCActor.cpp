a99c3272be9305d4597b725925f8b49c7d0fd35d
aaronvark <aoostdijk@gmail.com>
2021-10-13 16:49:13 +0200

    Re-added rtmidi through subodule, wip actor

---
+++ Actors/Midi2OSCActor.cpp
@@ -0,0 +1,90 @@
+//
+// Created by Aaron Oostdijk on 13/10/2021.
+//
+
+#include "Midi2OSCActor.h"
+
+const char * midi2OSCCapabilities =
+        "capabilities\n"
+        "    data\n"
+        "        name = \"deviceIndex\"\n"
+        "        type = \"string\"\n"
+        "        value = \"6200\"\n"
+        "        min = \"0\"\n"
+        "        max = \"10\"\n"
+        "        api_call = \"SET DEVICEINDEX\"\n"
+        "        api_value = \"s\"\n"           // optional picture format used in zsock_send
+        "outputs\n"
+        "    output\n"
+        "        type = \"OSC\"\n";
+
+zmsg_t * Midi2OSC::handleInit( sphactor_event_t *ev )
+{
+    // RtMidiIn constructor
+    try {
+        midiin = new RtMidiIn();
+    }
+    catch ( RtMidiError &error ) {
+        zsys_info("RTMIDI ERROR: %s", error.getMessage().c_str());
+    }
+
+    // Check inputs.
+    unsigned int nPorts = midiin->getPortCount();
+    zsys_info("There are %i MIDI input sources available.", nPorts);
+    std::string portName;
+    for ( unsigned int i=0; i<nPorts; i++ ) {
+        try {
+            portName = midiin->getPortName(i);
+        }
+        catch ( RtMidiError &error ) {
+            zsys_info("RTMIDI ERROR: %s", error.getMessage().c_str());
+            break;
+        }
+        zsys_info("Input port %i: %s", i+1, portName.c_str());
+    }
+
+    sphactor_actor_set_capability((sphactor_actor_t*)ev->actor, zconfig_str_load(midi2OSCCapabilities));
+    return Sphactor::handleInit(ev);
+}
+
+zmsg_t * Midi2OSC::handleStop( sphactor_event_t *ev ) {
+    return Sphactor::handleStop(ev);
+}
+
+zmsg_t * Midi2OSC::handleAPI( sphactor_event_t *ev )
+{
+    char * cmd = zmsg_popstr(ev->msg);
+    if (cmd) {
+        /*
+        if ( streq(cmd, "SET PORT") ) {
+
+        }
+        */
+
+        zstr_free(&cmd);
+    }
+    return Sphactor::handleAPI(ev);
+}
+
+zmsg_t * Midi2OSC::handleCustomSocket( sphactor_event_t *ev )
+{
+    assert(ev->msg);
+    zmsg_t *retmsg = NULL;
+
+
+    zframe_t *frame = zmsg_pop(ev->msg);
+    if (zframe_size(frame) == sizeof( void *) )
+    {
+        void *p = *(void **)zframe_data(frame);
+        /*
+        if ( zsock_is( p ) && p == this->dgramr )
+        {
+
+        }
+        */
+    }
+    zframe_destroy(&frame);
+
+
+    return retmsg;
+}
---
+++ Actors/Midi2OSCActor.h
@@ -0,0 +1,26 @@
+//
+// Created by Aaron Oostdijk on 13/10/2021.
+//
+
+#ifndef GAZEBOSC_MIDI2OSCACTOR_H
+#define GAZEBOSC_MIDI2OSCACTOR_H
+
+#include "libsphactor.hpp"
+#include "../ext/rtmidi/RtMidi.h"
+
+class Midi2OSC : public Sphactor {
+private:
+    RtMidiIn  *midiin = 0;
+
+public:
+    Midi2OSC() : Sphactor() {
+
+    }
+
+    zmsg_t * handleInit( sphactor_event_t *ev );
+    zmsg_t * handleStop( sphactor_event_t *ev );
+    zmsg_t * handleCustomSocket( sphactor_event_t *ev );
+    zmsg_t * handleAPI( sphactor_event_t *ev );
+};
+
+#endif //GAZEBOSC_MIDI2OSCACTOR_H
