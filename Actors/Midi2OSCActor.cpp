e2257cbb53cdb0c7e610603faa80723c8a752c95
aaronvark <aoostdijk@gmail.com>
2021-10-19 16:42:05 +0200

    Implemented MIDI parsing & OSC building

--- Actors/Midi2OSCActor.cpp
+++ Actors/Midi2OSCActor.cpp
@@ -4,23 +4,29 @@
 
 #include "Midi2OSCActor.h"
 
-const char * midi2OSCCapabilities =
+const char * Midi2OSC::capabilities =
         "capabilities\n"
         "    data\n"
-        "        name = \"deviceIndex\"\n"
-        "        type = \"string\"\n"
-        "        value = \"0\"\n"
-        "        min = \"0\"\n"
-        "        max = \"10\"\n"
-        "        api_call = \"SET DEVICEINDEX\"\n"
-        "        api_value = \"s\"\n"           // optional picture format used in zsock_send
-        "    data\n"
         "        name = \"activePort\"\n"
-        "        type = \"string\"\n"
+        "        type = \"int\"\n"
         "        value = \"0\"\n"
         "        min = \"0\"\n"
         "        max = \"10\"\n"
         "        api_call = \"SET PORT\"\n"
+        "        api_value = \"i\"\n"           // optional picture format used in zsock_send
+        "    data\n"
+        "        name = \"timeout\"\n"
+        "        type = \"int\"\n"
+        "        value = \"16\"\n"
+        "        min = \"1\"\n"
+        "        max = \"10000\"\n"
+        "        api_call = \"SET TIMEOUT\"\n"
+        "        api_value = \"i\"\n"           // optional picture format used in zsock_send
+        "    data\n"
+        "        name = \"sendRaw\"\n"
+        "        type = \"bool\"\n"
+        "        value = \"False\"\n"
+        "        api_call = \"SET RAW\"\n"
         "        api_value = \"s\"\n"           // optional picture format used in zsock_send
         "outputs\n"
         "    output\n"
@@ -36,28 +42,136 @@ zmsg_t * Midi2OSC::handleInit( sphactor_event_t *ev )
         zsys_info("RTMIDI ERROR: %s", error.getMessage().c_str());
     }
 
-    // Check inputs.
+    // Check input count
     unsigned int nPorts = midiin->getPortCount();
     zsys_info("There are %i MIDI input sources available.", nPorts);
+
+    // Get ports, store in list
     std::string portName;
     for ( unsigned int i=0; i<nPorts; i++ ) {
         try {
             portName = midiin->getPortName(i);
+            portList.push_back(portName);
         }
         catch ( RtMidiError &error ) {
             zsys_info("RTMIDI ERROR: %s", error.getMessage().c_str());
             break;
         }
-        zsys_info("Input port %i: %s", i+1, portName.c_str());
     }
 
-    //TODO: Get devices, store in list, alter max-capability for deviceIndex
+    midiin->openPort(0);
 
-    sphactor_actor_set_capability((sphactor_actor_t*)ev->actor, zconfig_str_load(midi2OSCCapabilities));
     return Sphactor::handleInit(ev);
 }
 
+zmsg_t * Midi2OSC::handleTimer( sphactor_event_t *ev ) {
+
+    if ( midiin->isPortOpen() ) {
+        double read = midiin->getMessage(&messageBuffer);
+        if ( read > 0 ) {
+            // message values
+            int status = 0, channel = 0, pitch = 0, velocity = 0, control = 0, value = 0;
+
+            // parse (taken from ofxMidi)
+            if(messageBuffer[0] >= MIDI_SYSEX) {
+                status = (MidiStatus) (messageBuffer[0] & 0xFF);
+                channel = 0;
+            } else {
+                status = (MidiStatus) (messageBuffer[0] & 0xF0);
+                channel = (int) (messageBuffer[0] & 0x0F)+1;
+            }
+            switch(status) {
+                case MIDI_NOTE_ON:
+                case MIDI_NOTE_OFF:
+                    pitch = (int) messageBuffer[1];
+                    velocity = (int) messageBuffer[2];
+                    break;
+                case MIDI_CONTROL_CHANGE:
+                    control = (int) messageBuffer[1];
+                    value = (int) messageBuffer[2];
+                    break;
+                case MIDI_PROGRAM_CHANGE:
+                case MIDI_AFTERTOUCH:
+                    value = (int) messageBuffer[1];
+                    break;
+                case MIDI_PITCH_BEND:
+                    value = (int) (messageBuffer[2] << 7) + (int) messageBuffer[1]; // msb + lsb
+                    break;
+                case MIDI_POLY_AFTERTOUCH:
+                    pitch = (int) messageBuffer[1];
+                    value = (int) messageBuffer[2];
+                    break;
+                case MIDI_SONG_POS_POINTER:
+                    value = (int) (messageBuffer[2] << 7) + (int) messageBuffer[1]; // msb + lsb
+                    break;
+                default:
+                    break;
+            }
+
+            // zsys_info( "%i, %i, %i, %i, %i, %i", status, channel, pitch, velocity, control, value );
+
+            std::string address;
+            zosc_t * oscMsg = nullptr;
+
+            // Put into OSC Message & return, taken from NatNet2OSCBridge
+            if ( sendRaw )
+            {
+                address = "/" + portList[activePort];
+                oscMsg = zosc_create(address.c_str(), "ccc", messageBuffer[0], messageBuffer[1], messageBuffer[2]);
+            }
+            else if (status < MIDI_SYSEX) {
+                address = "/" + portList[activePort] + "/" + std::to_string(channel) + "/" +
+                          getStatusString((MidiStatus) status);
+                switch (status) {
+                    case MIDI_NOTE_OFF:
+                    case MIDI_NOTE_ON:
+                        address.append("/" + std::to_string(pitch));
+                        oscMsg = zosc_create(address.c_str(), "i", velocity);
+                        break;
+                    case MIDI_CONTROL_CHANGE:
+                        address.append("/" + std::to_string(control));
+                        oscMsg = zosc_create(address.c_str(), "i", value);
+                        break;
+                    case MIDI_PROGRAM_CHANGE:
+                    case MIDI_AFTERTOUCH: // aka channel pressure
+                        oscMsg = zosc_create(address.c_str(), "i", value);
+                        break;
+                    case MIDI_PITCH_BEND:
+                        oscMsg = zosc_create(address.c_str(), "i", value);
+                        break;
+                    case MIDI_POLY_AFTERTOUCH: // aka key pressure
+                        address.append("/" + std::to_string(pitch));
+                        oscMsg = zosc_create(address.c_str(), "i", value);
+                        break;
+                    case MIDI_SONG_POS_POINTER:
+                        oscMsg = zosc_create(address.c_str(), "i", value);
+                        break;
+                    default:
+                        zsys_info("UNHANDLED MIDI STATUS: %i", status);
+                        break;
+                }
+            }
+
+            if ( oscMsg != nullptr ) {
+                zmsg_t * retMsg = zmsg_new();
+                zmsg_add(retMsg, zosc_packx(&oscMsg));
+
+                return retMsg;
+            }
+        }
+    }
+
+    zmsg_destroy(&ev->msg);
+    return nullptr;
+}
+
 zmsg_t * Midi2OSC::handleStop( sphactor_event_t *ev ) {
+    // Clean up RtMidi instance
+    if ( midiin->isPortOpen() ) {
+        midiin->closePort();
+    }
+    delete midiin;
+
     return Sphactor::handleStop(ev);
 }
 
@@ -69,45 +183,29 @@ zmsg_t * Midi2OSC::handleAPI( sphactor_event_t *ev )
 
         if ( streq(cmd, "SET PORT") ) {
             zsys_info("SET PORT");
-            int newPort = 0;
-            if ( activePort != newPort ) {
-                if ( midiin->isPortOpen()) {
-                    midiin->closePort();
+            char* ifChar = zmsg_popstr(ev->msg);
+            int newPort = std::stoi(ifChar);
+            zstr_free(&ifChar);
+
+            if ( newPort < portList.size() ) {
+                if ( activePort != newPort ) {
+                    if ( midiin->isPortOpen()) {
+                        midiin->closePort();
+                    }
+                    activePort = newPort;
+                    midiin->openPort(activePort);
                 }
-
-                activePort = newPort;
-
-                midiin->openPort(activePort);
             }
-
+            else {
+                zsys_info( "INVALID PORT NUMBER: %i", newPort);
+            }
         }
-
+        else if ( streq(cmd, "SET RAW") ) {
+            char * value = zmsg_popstr(ev->msg);
+            sendRaw = streq( value, "True");
+        }
 
         zstr_free(&cmd);
     }
     return Sphactor::handleAPI(ev);
-}
-
-zmsg_t * Midi2OSC::handleCustomSocket( sphactor_event_t *ev )
-{
-    assert(ev->msg);
-    zmsg_t *retmsg = NULL;
-
-
-    zframe_t *frame = zmsg_pop(ev->msg);
-    if (zframe_size(frame) == sizeof( void *) )
-    {
-        // TODO: Handle incoming midi, convert to OSC & return
-        void *p = *(void **)zframe_data(frame);
-        /*
-        if ( zsock_is( p ) && p == this->dgramr )
-        {
-
-        }
-        */
-    }
-    zframe_destroy(&frame);
-
-
-    return retmsg;
 }
--- Actors/Midi2OSCActor.h
+++ Actors/Midi2OSCActor.h
@@ -8,20 +8,104 @@
 #include "libsphactor.hpp"
 #include "../ext/rtmidi/RtMidi.h"
 
+// MIDI status bytes
+enum MidiStatus {
+    MIDI_UNKNOWN            = 0x00,
+
+    // channel voice messages
+    MIDI_NOTE_OFF           = 0x80,
+    MIDI_NOTE_ON            = 0x90,
+    MIDI_CONTROL_CHANGE     = 0xB0,
+    MIDI_PROGRAM_CHANGE     = 0xC0,
+    MIDI_PITCH_BEND         = 0xE0,
+    MIDI_AFTERTOUCH         = 0xD0, // aka channel pressure
+    MIDI_POLY_AFTERTOUCH    = 0xA0, // aka key pressure
+
+    // system messages
+    MIDI_SYSEX              = 0xF0,
+    MIDI_TIME_CODE          = 0xF1,
+    MIDI_SONG_POS_POINTER   = 0xF2,
+    MIDI_SONG_SELECT        = 0xF3,
+    MIDI_TUNE_REQUEST       = 0xF6,
+    MIDI_SYSEX_END          = 0xF7,
+    MIDI_TIME_CLOCK         = 0xF8,
+    MIDI_START              = 0xFA,
+    MIDI_CONTINUE           = 0xFB,
+    MIDI_STOP               = 0xFC,
+    MIDI_ACTIVE_SENSING     = 0xFE,
+    MIDI_SYSTEM_RESET       = 0xFF
+};
+
+// number range defines
+// because it's sometimes hard to remember these  ...
+#define MIDI_MIN_BEND       0
+#define MIDI_MAX_BEND       16383
+
 class Midi2OSC : public Sphactor {
 private:
     RtMidiIn  *midiin = 0;
     int activePort = 0;
+    bool sendRaw = false;
+
+    std::vector<std::string> portList;
+    std::vector<unsigned char> messageBuffer;
 
 public:
+    static const char *capabilities;
+
     Midi2OSC() : Sphactor() {
 
     }
 
     zmsg_t * handleInit( sphactor_event_t *ev );
     zmsg_t * handleStop( sphactor_event_t *ev );
-    zmsg_t * handleCustomSocket( sphactor_event_t *ev );
+    zmsg_t* handleTimer(sphactor_event_t *ev);
     zmsg_t * handleAPI( sphactor_event_t *ev );
+
+    std::string getStatusString(MidiStatus status) {
+        switch (status) {
+            case MIDI_NOTE_OFF:
+                return "Note_Off";
+            case MIDI_NOTE_ON:
+                return "Note_On";
+            case MIDI_CONTROL_CHANGE:
+                return "Control_Change";
+            case MIDI_PROGRAM_CHANGE:
+                return "Program_Change";
+            case MIDI_PITCH_BEND:
+                return "Pitch_Bend";
+            case MIDI_AFTERTOUCH:
+                return "Aftertouch";
+            case MIDI_POLY_AFTERTOUCH:
+                return "Poly_Aftertouch";
+            case MIDI_SYSEX:
+                return "Sysex";
+            case MIDI_TIME_CODE:
+                return "Time_Code";
+            case MIDI_SONG_POS_POINTER:
+                return "Song_Pos";
+            case MIDI_SONG_SELECT:
+                return "Song_Select";
+            case MIDI_TUNE_REQUEST:
+                return "Tune_Request";
+            case MIDI_SYSEX_END:
+                return "Sysex_End";
+            case MIDI_TIME_CLOCK:
+                return "Time_Clock";
+            case MIDI_START:
+                return "Start";
+            case MIDI_CONTINUE:
+                return "Continue";
+            case MIDI_STOP:
+                return "Stop";
+            case MIDI_ACTIVE_SENSING:
+                return "Active_Sensing";
+            case MIDI_SYSTEM_RESET:
+                return "System_Reset";
+            default:
+                return "Unknown";
+        }
+    }
 };
 
 #endif //GAZEBOSC_MIDI2OSCACTOR_H
