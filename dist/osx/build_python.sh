d1170203422966c273dad88efb20c294defb3023
Arnaud Loonstra <arnaud@sphaero.org>
2023-09-13 21:08:11 +0200

    test using relocatable-python instead of self building

--- dist/osx/build_python.sh
+++ dist/osx/build_python.sh
@@ -7,6 +7,23 @@ else
     BUILD_DIR=$1
 fi
 
+if [[ -z "$2" ]]
+then
+    PY_VER=3.9.5
+else
+    PY_VER=$2
+fi
+
+
+if [ true ]
+then
+    git clone https://github.com/gregneagle/relocatable-python.git
+    cd relocatable-python
+    ./make_relocatable_python_framework.py --python-version $PY_VER --os-version 11 --destination $BUILD_DIR
+    exit $?
+fi
+
+# old code which we need to cleanup as this script never reaches here
 if [ -e cpython ]
 then
   cd cpython
